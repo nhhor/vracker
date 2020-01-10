172ff8807426a5f2787f970d773776763b2d9207
nhhor <nhhor@post.com>
2020-01-10 09:29:35 -0800

    add Project.find . Test 7 auto-passed. Test 8 passing.

--- lib/project.rb
+++ lib/project.rb
@@ -27,4 +27,11 @@ class Project
     projects
   end
 
+  def self.find(id)
+    project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
+    title = project.fetch("title")
+    id = project.fetch("id").to_i
+    Project.new({:title => title, :id => id})
+  end
+
 end
