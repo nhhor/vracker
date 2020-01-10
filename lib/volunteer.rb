eada2318672e61faf9e65951303ca83ba6a124f5
nhhor <nhhor@post.com>
2020-01-10 10:17:44 -0800

    add Volunteer.. .update and .delete methods.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -37,4 +37,14 @@ class Volunteer
     Volunteer.new({:name => name, :project_id => project_id, :id => id})
   end
 
+  def update(name, project_id)
+    @name = name
+    @project_id = project_id
+    DB.exec("UPDATE volunteers SET name = '#{@name}', project_id = #{@project_id} WHERE id = #{@id};")
+  end
+
+  def delete
+    DB.exec("DELETE FROM volunteers WHERE id = #{@id};")
+  end
+
 end
